2339fddd9a2b2bf650260c5109e1269f690ee675
Ismaiel Amr <ismaielnounou1@gmail.com>
2022-10-31 09:27:10 +0200

    Update adminscontroller.js

--- acltest/backend/controllers/adminscontroller.js
+++ acltest/backend/controllers/adminscontroller.js
@@ -2,10 +2,12 @@ const Admin = require('../models/Admins')
 
 const createAdmin = async (req, res) => {
     const {name, password} = req.body
-
+    const w = await Admin.findOne({name:name})
+    if(w){
+        return
+    }
     try{
-        
-        const admin = await Admin.create({name,password})
+    const admin = await Admin.create({name,password})
     }
     catch (error) {
         res.status(400).json({error: error.message})
