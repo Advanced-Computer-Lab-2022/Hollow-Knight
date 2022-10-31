7e7bed8245ec22176d11af1497253864a6b7aab9
MohamedHatem-0110 <83113802+MohamedHatem-0110@users.noreply.github.com>
2022-10-31 21:24:39 +0200

    final

--- acltest/frontend/src/components/ViewCoursesTable.js
+++ acltest/frontend/src/components/ViewCoursesTable.js
@@ -5,7 +5,8 @@ const CoursesTable = ({courses}) =>{
         <div className="coursesTable">
             <p><strong>Course Title:</strong>{courses.title} &nbsp;&nbsp;
             <strong>Total Hours:</strong>{courses.hours} &nbsp;&nbsp;
-            <strong>Course:</strong>{courses.rating}
+            <strong>Course rating:</strong>{courses.rating}&nbsp;&nbsp;
+            <strong>Price:</strong>
             <a href={URL}>
             <button>View Price</button>
             </a>
