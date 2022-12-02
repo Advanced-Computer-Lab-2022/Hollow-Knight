d2833ed7565ee5692f3d0189473698ef3f763561
MohamedHatem-0110 <83113802+MohamedHatem-0110@users.noreply.github.com>
2022-12-02 19:42:23 +0200

    Update coursescontroller.js

--- acltest/backend/controllers/coursescontroller.js
+++ acltest/backend/controllers/coursescontroller.js
@@ -1,30 +1,10 @@
 const { default: mongoose } = require("mongoose");
 const Course = require("../models/Courses");
 
-const createCourse = async (req, res) => {
-  const { title, price, subject, author, rating, hours } = req.body;
-
-  try {
-    const course = await Course.create({
-      title,
-      price,
-      subject,
-      author,
-      rating,
-      hours,
-    });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-
-  res.json({ mssg: "post new course" });
-};
-
 const findCourses = async (req, res) => {
   const courses = await Course.find({});
   res.status(200).json(courses);
 };
 module.exports = {
-  createCourse,
   findCourses,
 };
