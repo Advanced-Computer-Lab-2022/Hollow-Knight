a0f54e0acc30fa1a659971d939c3a83297e99ae5
Mohamed Hatem <83113802+MohamedHatem-0110@users.noreply.github.com>
2023-01-08 22:17:28 +0200

    Update Home

--- acltest/frontend/src/pages/Home.js
+++ acltest/frontend/src/pages/Home.js
@@ -1,48 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return (
-    <div className="home">
-      <div>
-        <h2>Home</h2>
-      </div>
-      <div>
+  const navigate = useNavigate();
+  useEffect(()=>{  navigate("/login");},[])
 
-
-       <Link to="/adminhome">Admin Home</Link>
-      
-        <br></br>
-        <Link to="/viewallcourseswithprices">View All Courses With Prices</Link>
-        <br></br>
-        <Link to="/selectcountry">Select a Country</Link>
-        <br></br>
-        <Link to="/updateinfo">
-        Update Instructor's information
-        </Link>
-
-        <br></br>
-        
-        <Link to="/resetpassword">
-        Reset Password
-        </Link> 
-        <br></br>
-      <Link to="/InstructorHome">
-        Instructor Home Page
-        </Link> 
-        <br></br>
-        <Link to="/trainee">
-        Trainee Home Page
-        </Link> 
-        <Link to="/userAggrement">
-        View All Courses
-        </Link>
-        <br></br>
-      
-        </div>
-        </div>
-    )
 }
 
 export default Home
