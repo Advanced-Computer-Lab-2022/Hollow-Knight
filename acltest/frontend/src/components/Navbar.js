8e74641d624d0790f2d400f96c62dc8c3f45fa28
Mohamed Hatem <83113802+MohamedHatem-0110@users.noreply.github.com>
2023-01-08 22:32:49 +0200

    logout nav

--- acltest/frontend/src/components/Navbar.js
+++ acltest/frontend/src/components/Navbar.js
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
 import { useAuthContext } from "../hooks/useAuthContext";
-
+import { useNavigate } from "react-router-dom";
 const Navbar = () => {
   const { logout } = useLogout();
   const { user } = useAuthContext();
+  const navigate = useNavigate();
   const handleClick = () => {
     logout()
+    navigate("/login")
   }
   return (
     <header>
