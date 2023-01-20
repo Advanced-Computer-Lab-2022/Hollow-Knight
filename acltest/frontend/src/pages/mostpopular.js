b081ed19fd54964e641991c089469fb57cc8584b
mostaf7583 <73074293+mostaf7583@users.noreply.github.com>
2023-01-20 23:32:14 +0200

    wda

--- acltest/frontend/src/pages/mostpopular.js
+++ acltest/frontend/src/pages/mostpopular.js
@@ -1,7 +1,17 @@
 
 import React, { useState, useEffect } from "react";
 import { useAuthContext } from "../hooks/useAuthContext";
-
+import { Grid } from "@mui/material";
+import { Card } from "@mui/material";
+import { Container } from "@mui/system";
+import { Typography } from "@mui/material";
+import { Stack } from "@mui/material";
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
+import { Box } from "@mui/system";
+import { TextField } from "@mui/material";
+import { FormControl } from "@mui/material";
+import Embed2 from "../components/Embed2"
 
 
 const MostPopular = () => {
@@ -32,18 +42,74 @@ return (
 <div className="courses">
 {mostPopular &&
 mostPopular.map((course) => (
-<div key={course._id}>
-<p>
-<strong>Course Title:</strong>
-{course.title} &nbsp;&nbsp;
-<strong>Total Hours:</strong>
-{course.total_hours} &nbsp;&nbsp;
-<strong>Course Rating:</strong>
-{course.overallRating}
-</p>
-</div>
-))}
+    <Grid item  key={course._id} xs={6}>
+      <p>
+      
+        
+      <Card 
+      
+      sx={{marginBottom:2,marginTop:4 ,borderRadius:3, width:550,height:660}}>
+      <Container  sx={{marginTop:5,marginBottom:3}}>
+      <Typography
+      align="center"  
+      sx={{fontSize:30,marginBottom:2}}>
+        <strong>{course.title}</strong>
+         &nbsp;&nbsp;
+        </Typography>
+        
+        <Typography 
+        align="center" 
+         sx={{fontSize:22,marginBottom:1}}>
+        Total Hours : {course.total_hours} &nbsp;&nbsp;
+        </Typography>
+        
+        <Typography  
+        align="center" 
+        sx={{fontSize:25,marginBottom:0}}>
+        Rating : {course.overallRating}
+        </Typography>
+
+         
+        <Typography  
+        align="center" 
+        sx={{fontSize:25,marginBottom:0}}>
+        Instructor : {course.name}
+        </Typography>
+
+        <Stack direction="row" spacing={2} sx={{marginLeft:24,marginTop:1}}>
+        <Typography  
+        align="center" 
+        sx={{fontSize:25,marginBottom:2,marginTop:-0.5}}>
+        Price : {course.price}
+        </Typography>
+        <Typography    align="center"  sx={{fontSize:22,marginTop:9}}> 
+        </Typography>
+        </Stack>
+        {course.video &&
+        <Embed2 embedId={course.video} />
+        }
+        </Container>
+
+        <Button variant="contained" sx={{marginLeft:40,marginBottom:4}} onClick={() => window.location.href= `/traineesearchcourse?courseId=`+course._id} >View Details</Button>
+        </Card>
+        
+     
+        
+      </p>
+
+      </Grid>
+   
+  ))
+  }
 </div>
 );
 };
+
+
+
+  
+
+
+
+
 export default MostPopular;
